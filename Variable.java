f581b89d1652ca96205dee2748c66e8669b471bf
ryandra1222 <115127511+ryandra1222@users.noreply.github.com>
2022-11-24 12:53:51 +0700

    Create Variable.java

---
+++ Variable.java
@@ -0,0 +1,11 @@
+public class Variable{
+    public static void main(String args[]) {
+         String salam = "Assalamualaikum";
+         String nama = "Ryandra Putra";
+         int lahir = 2003;
+         
+         System.out.println(salam);
+         System.out.println("Nama saya "+nama);
+         System.out.println("Saya lahir di tahun : "+lahir);
+    }
+}
